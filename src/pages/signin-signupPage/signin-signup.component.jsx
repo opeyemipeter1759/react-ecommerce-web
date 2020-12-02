637b580e2b2712f65e3ba2ced6aa1b421557107b
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2020-12-02 03:38:38 -0800

    firebase Authentication

--- src/pages/signin-signupPage/signin-signup.component.jsx
+++ src/pages/signin-signupPage/signin-signup.component.jsx
@@ -1,11 +1,13 @@
 import react from 'react';
 import './signin-signup.scss';
 
-import SignIn from '../../components/signin/sign-in.component'
+import SignIn from '../../components/signin/sign-in.component';
+import SignUp from '../../components/signup/signup.component'
 
 const SignInSignUp =()=>(
-<div className="sign-in">
+<div className="signin-signup">
   <SignIn/>
+  <SignUp/>
 </div>
 
 )
